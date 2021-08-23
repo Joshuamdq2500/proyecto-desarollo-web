8b34dd2527f52a74434ad9a6323a4bdcab5f16ff
Joshuamdq2500 <joshualucasbattiato5@gmail.com>
2021-08-23 14:42:18 -0300

    subida a git hub de proyecto

---
+++ index.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal();
+sr.reveal (".navbar",{
+duration: 3000
+
+});
+window.sr = ScrollReveal();
+sr.reveal(".texto__1", {
+    duration: 3000
+
+});
+window.sr = ScrollReveal();
+sr.reveal(".texto__2", {
+    duration: 3000
+
+});
+
+window.sr = ScrollReveal();
+sr.reveal(".texton_N", {
+    duration: 3000
+});
+window.sr = ScrollReveal();
+sr.reveal(".textitoUno", {
+    duration: 2000
+});
+window.sr = ScrollReveal();
+sr.reveal(".textitoDos", {
+    duration: 2000
+});
+window.sr = ScrollReveal();
+sr.reveal(".textitoTres", {
+    duration: 2000
+});
+window.sr = ScrollReveal();
+sr.reveal(".texton_N", {
+    duration: 3000
+});
+window.sr = ScrollReveal();
+sr.reveal(".texton_N", {
+    duration: 3000
+});
